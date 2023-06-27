58726a08ee59e62a5d4460117d11091ca1e6c046
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-06-27 19:57:56 +0530

    feat: routing and navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
-import Header from "./layouts/Header/Header";
+import { Bookmark, Explore, Home } from "./pages";
 import "./normalize.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="bookmark" element={<Bookmark />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navigation from "./Navigation/Navigation";
+import Feeds from "./Feeds/Feeds";
+import Suggested from "./Suggested/Suggested";
+import Header from "./Header/Header";
+export { Navigation, Feeds, Suggested, Header };
