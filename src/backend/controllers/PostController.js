a10414809e8781d2838e25161c4a17d09ca1f90e
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2023-07-02 18:19:14 +0530

    fix: backend bug

--- src/backend/controllers/PostController.js
+++ src/backend/controllers/PostController.js
@@ -42,9 +42,9 @@ export const getPostHandler = function (schema, request) {
  * */
 
 export const getAllUserPostsHandler = function (schema, request) {
-  const { username } = request.params;
+  const { userId } = request.params;
   try {
-    const posts = schema.posts.where({ username })?.models;
+    const posts = schema.posts.where({ userId })?.models;
     return new Response(200, {}, { posts });
   } catch (error) {
     return new Response(
@@ -87,6 +87,7 @@ export const createPostHandler = function (schema, request) {
         dislikedBy: [],
       },
       username: user.username,
+      userId: user._id,
       createdAt: formatDate(),
       updatedAt: formatDate(),
     };
@@ -167,7 +168,16 @@ export const likePostHandler = function (schema, request) {
         }
       );
     }
-    const { _id, firstName, lastName, username, createdAt, updatedAt, followers, following } = user;
+    const {
+      _id,
+      firstName,
+      lastName,
+      username,
+      createdAt,
+      updatedAt,
+      followers,
+      following,
+    } = user;
     const postId = request.params.postId;
     const post = schema.posts.findBy({ _id: postId }).attrs;
     if (post.likes.likedBy.some((currUser) => currUser._id === user._id)) {
@@ -181,7 +191,16 @@ export const likePostHandler = function (schema, request) {
       (currUser) => currUser._id !== user._id
     );
     post.likes.likeCount += 1;
-    post.likes.likedBy.push({ _id, firstName, lastName, username, createdAt, updatedAt, followers, following });
+    post.likes.likedBy.push({
+      _id,
+      firstName,
+      lastName,
+      username,
+      createdAt,
+      updatedAt,
+      followers,
+      following,
+    });
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
   } catch (error) {
